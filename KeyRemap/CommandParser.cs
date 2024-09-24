b742cd665f58637ea2f9259678f8fd8ceb782fe8
jordonkane <kanejord99@outlook.com>
2024-09-24 19:36:36 -0400

    [Implementation] Added Command Parser

---
+++ KeyRemap/CommandParser.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyRemap
+{
+    class CommandParser
+    {
+        public static void Parse(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                DisplayHelp();
+                return;
+            }
+
+            switch (args[0].ToLower())
+            {
+                case "remap":
+                    break;
+                case "reset":
+                    break;
+                case "list-remapped":
+                    break;
+                default:
+                    DisplayHelp();
+                    break;
+            }
+        }
+
+        private static void DisplayHelp()
+        {
+            Console.WriteLine("Usage:");
+            Console.WriteLine("  keyremap.exe remap            - Start the key remapping process.");
+            Console.WriteLine("  keyremap.exe list-remapped    - List all remapped keys.");
+            Console.WriteLine("  keyremap.exe reset            - Reset all key mappings to default.");
+        }
+    }
+
+}
