8cf513a464b5783d35be75bffdba975345098976
jordonkane <kanejord99@outlook.com>
2024-09-25 10:50:54 -0400

    [Unit Testing] Create Moq Tests for RegistryManager

---
+++ KeyRemap.Tests/RegistryManagerTests.cs
@@ -0,0 +1,31 @@
+﻿using KeyRemap.Interface;
+using Moq;
+using Xunit;
+
+namespace KeyRemap.Tests
+{
+    public class RegistryManagerTests
+    {
+        private Mock<IRegistryManager> _mockRegistryManager;
+
+        public RegistryManagerTests()
+        {
+            _mockRegistryManager = new Mock<IRegistryManager>();
+        }
+
+        [Fact]
+        public void RemapKeyInRegistry_ShouldInvokeRegistryMethod()
+        {
+            var mockScancodeMap = new byte[] { 0x00, 0x00, 0x00, 0x00 };
+            _mockRegistryManager.Object.RemapKeyInRegistry(mockScancodeMap);
+            _mockRegistryManager.Verify(rm => rm.RemapKeyInRegistry(It.IsAny<byte[]>()), Times.Once);
+        }
+
+        [Fact]
+        public void ResetKeyMappings_ShouldInvokeResetRegistryMethod()
+        {
+            _mockRegistryManager.Object.ResetKeyMappings();
+            _mockRegistryManager.Verify(rm => rm.ResetKeyMappings(), Times.Once);
+        }
+    }
+}
